0af261cb453f3b62e2e1c7924bbf897991eb4874
rsolinas <rodrigosolinas@gmail.com>
2023-11-30 13:10:22 -0300

    Activación de multisitio
    Eso es todo.

--- config/application.php
+++ config/application.php
@@ -11,6 +11,15 @@
 use Roots\WPConfig\Config;
 use function Env\env;
 
+/* Multisite */
+Config::define('WP_ALLOW_MULTISITE', true);
+Config::define('MULTISITE', true);
+Config::define('SUBDOMAIN_INSTALL', true); // Set to true if using subdomains
+Config::define('DOMAIN_CURRENT_SITE', env('DOMAIN_CURRENT_SITE'));
+Config::define('PATH_CURRENT_SITE', env('PATH_CURRENT_SITE') ?: '/');
+Config::define('SITE_ID_CURRENT_SITE', env('SITE_ID_CURRENT_SITE') ?: 1);
+Config::define('BLOG_ID_CURRENT_SITE', env('BLOG_ID_CURRENT_SITE') ?: 1);
+
 /**
  * Directory containing all of the site's files
  *
